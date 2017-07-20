25192ccbde03c73adee344b0b09c3d47b3b148c9
Kai Konen <kkonen@techfak.uni-bielefeld.de>
2017-07-20 17:01:56 +0200

    removed unused code

--- src/PepperImageSink.cpp
+++ src/PepperImageSink.cpp
@@ -94,35 +94,6 @@ namespace pepper_image_sink {
         }
 
         void depth_cb(const sensor_msgs::CompressedImage::ConstPtr &message) {
-
-//            cv_bridge::CvImagePtr cv_ptr(new cv_bridge::CvImage);
-//
-//            // Copy message header
-//            d_cv_ptr->header = message->header;
-//
-//            // Assign image encoding
-//            std::string image_encoding = message->format.substr(0, message->format.find(';'));
-//            d_cv_ptr->encoding = image_encoding;
-//            std::vector <uint8_t> imageData;
-//
-//            ROS_DEBUG("Copying data");
-//
-//            for (int i = 0; i < message->data.size(); i++) {
-//                imageData.push_back(message->data[i]);
-//            }
-//            try {
-//                d_cv_ptr->image = cv::imdecode(imageData, CV_LOAD_IMAGE_UNCHANGED);
-//            }
-//            catch (cv::Exception &e) {
-//                ROS_ERROR("%s", e.what());
-//            }
-//
-//            size_t rows = d_cv_ptr->image.rows;
-//            size_t cols = d_cv_ptr->image.cols;
-//
-//            if ((rows > 0) && (cols > 0)) {
-//                d_pub.publish(d_cv_ptr->toImageMsg());
-//            }
             try {
                 d_cv_ptr = cv_bridge::toCvCopy(message, "mono16");
             }
