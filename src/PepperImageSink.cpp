368b8ada10724d6f30091a304238def2732202a5
Kai Konen <kkonen@techfak.uni-bielefeld.de>
2017-07-20 13:31:12 +0200

    removed depth

--- src/PepperImageSink.cpp
+++ src/PepperImageSink.cpp
@@ -85,7 +85,7 @@ namespace pepper_image_sink {
 
         void depth_cb(const sensor_msgs::CompressedImage::ConstPtr &input) {
             try {
-                d_cv_ptr = cv_bridge::toCvCopy(input,"mono16");
+                d_cv_ptr = cv_bridge::toCvCopy(input);
             }
             catch (cv_bridge::Exception &e) {
                 ROS_ERROR("cv_bridge exception: %s", e.what());
